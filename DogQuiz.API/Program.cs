6390a4e520950735ba63f289ff90642e665079bf
herzkoenig <herzkoenig@xoxo.rip>
2024-11-16 22:00:15 +0100

    Added Keycloak to AppHosts, creates DogQuiz.Tests (xUnit) project, renamed Images folder to Breeds

--- DogQuiz.API/Program.cs
+++ DogQuiz.API/Program.cs
@@ -1,12 +1,17 @@
 using DogQuiz.API.Services;
 using DogQuiz.API.Services.Interfaces;
 using DogQuiz.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.FileProviders;
+using Microsoft.VisualBasic;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
+
+builder.Services.AddProblemDetails();
+
 
 builder.Services.AddDbContextPool<ApplicationDbContext>(options =>
 	options.UseSqlServer(builder.Configuration.GetConnectionString("db1"), sqlOptions =>
@@ -18,72 +23,45 @@ builder.EnrichSqlServerDbContext<ApplicationDbContext>(settings =>
 	// Disable Aspire default retries as we're using a custom execution strategy
 	settings.DisableRetry = true);
 
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthorization();
+builder.Services.AddScoped<IBreedService, BreedService>();
+
 var app = builder.Build();
 
+app.UseHttpsRedirection();
+app.UseStaticFiles(new StaticFileOptions
+{
+	FileProvider = new PhysicalFileProvider(
+		Path.Combine(Directory.GetCurrentDirectory(), "..", "DogQuiz.Images", "Breeds")),
+	RequestPath = "/images"
+});
+
+if (!app.Environment.IsDevelopment())
+{
+	app.UseExceptionHandler("/Error");
+	app.UseHsts();
+}
+
+if (app.Environment.IsDevelopment())
+{
+	app.UseSwagger();
+	app.UseSwaggerUI();
+}
+
+// Routing setup (must be before Authentication and Authorization)
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+//app.MapIdentityApi<User>();
+app.MapControllers();
+app.MapFallbackToFile("/index.html");
 app.MapDefaultEndpoints();
 
 app.Run();
-
-/* The above configuration is intended to get migrations fully working; *
- * later, switch back to the previous configuration below.				*/
-
-//using DogQuiz.API.Services;
-//using DogQuiz.API.Services.Interfaces;
-//using DogQuiz.Data;
-//using Microsoft.EntityFrameworkCore;
-//using Microsoft.Extensions.FileProviders;
-
-//var builder = WebApplication.CreateBuilder(args);
-
-//builder.AddServiceDefaults();
-
-//builder.Services.AddDbContextPool<ApplicationDbContext>(options =>
-//	options.UseSqlServer(builder.Configuration.GetConnectionString("db1"), sqlOptions =>
-//	{
-//		// Workaround for https://github.com/dotnet/aspire/issues/1023
-//		sqlOptions.ExecutionStrategy(c => new RetryingSqlServerRetryingExecutionStrategy(c));
-//	}));
-//builder.EnrichSqlServerDbContext<ApplicationDbContext>(settings =>
-//	// Disable Aspire default retries as we're using a custom execution strategy
-//	settings.DisableRetry = true);
-
-//builder.Services.AddControllers();
-//builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
-//builder.Services.AddAuthorization();
-//builder.Services.AddScoped<IBreedService, BreedService>();
-
-//var app = builder.Build();
-
-//app.UseHttpsRedirection();
-//app.UseStaticFiles(new StaticFileOptions
-//{
-//	FileProvider = new PhysicalFileProvider(
-//		Path.Combine(Directory.GetCurrentDirectory(), "Images")),
-//	RequestPath = "/images"
-//});
-
-//if (!app.Environment.IsDevelopment())
-//{
-//	app.UseExceptionHandler("/Error");
-//	app.UseHsts();
-//}
-
-//if (app.Environment.IsDevelopment())
-//{
-//	app.UseSwagger();
-//	app.UseSwaggerUI();
-//}
-
-//// Routing setup (must be before Authentication and Authorization)
-//app.UseRouting();
-
-//app.UseAuthentication();
-//app.UseAuthorization();
-
-////app.MapIdentityApi<User>();
-//app.MapControllers();
-//app.MapFallbackToFile("/index.html");
-//app.MapDefaultEndpoints();
-
-//app.Run();
--- DogQuiz.AppHost/Program.cs
+++ DogQuiz.AppHost/Program.cs
@@ -1,6 +1,14 @@
+// https://anthonysimmon.com/referencing-external-docker-containers-dotnet-aspire-custom-resources/
+// keycloak guide: https://fiodar.substack.com/p/hosting-a-keycloak-container-in-dotnet-aspire
+// https://apps-on-azure.net/dotnet/2024/2024-09-26-aspire-keycloak-part1.html
+
+using Aspire.Hosting;
+using Aspire.Hosting.Keycloak;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
 var sqlserver = builder.AddSqlServer("sqlserver")
+    //.WithDataVolume()
     .WithLifetime(ContainerLifetime.Persistent);
 
 var sqldb = sqlserver.AddDatabase("sqldb");
@@ -9,8 +17,11 @@ var migrationService = builder.AddProject<Projects.DogQuiz_MigrationService>("mi
     .WithReference(sqldb)
     .WaitFor(sqldb);
 
+var keycloak = builder.AddKeycloak("keycloak", 8080);
+
 builder.AddProject<Projects.DogQuiz_API>("api")
     .WithReference(sqldb)
+    .WithReference(keycloak)
     .WaitForCompletion(migrationService);
 
 builder.Build().Run();
